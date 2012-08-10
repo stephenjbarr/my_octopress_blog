5651f4226f2c4d54f4d89d324950fae98f386abb
Stephen Jeffrey Barr <stephenjbarr@gmail.com>
2012-08-09 23:46:25 -0700

    Added Some Old Posts

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/o/"
+http_images_path = "/o/images"
+http_fonts_path = "/o/fonts"
+css_dir = "public/o/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
